c9c2fc98596a401d9edff3d38c723a0583f6dc59
Priscila Leite <pricila135leite@gmail.com>
2023-05-01 15:50:03 -0300

    Criando as Entities Categoria e Catalogo

---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace APICatalogo.Model;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string?  Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+    
+
+}
