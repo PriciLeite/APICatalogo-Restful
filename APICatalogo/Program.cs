e9dee6446161d185070d363e26e835dcd73e2627
Priscila Leite <pricila135leite@gmail.com>
2023-05-01 16:34:08 -0300

    - Instalando as bibliotecas - Criando a classe Context para configurar as tabelas Categorias e  Produtos - Definindo a ConnectionStrings - Configurando o Serviço na Classe Program

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string? mySqlConnetion = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnetion,
+    ServerVersion.AutoDetect(mySqlConnetion)));
+
 
 var app = builder.Build();
 
