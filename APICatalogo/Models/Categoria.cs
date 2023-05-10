c5c2df629ce0a1dafb79e95e4232fd35f6c4e475
Priscila Leite <pricila135leite@gmail.com>
2023-05-10 20:09:52 -0300

    Criando as Migrations Aplicando as Data Anottation para os atributos das classes.

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,13 +1,22 @@
 ﻿using APICatalogo.Model;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models;
 
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
+
     public ICollection<Produto>? Produtos { get; set; } = new Collection<Produto>();
 
 }
