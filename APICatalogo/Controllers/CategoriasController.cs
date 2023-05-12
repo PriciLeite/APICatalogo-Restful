74c54d89eb09c7a4ca8f6b2d682504f7a7b5f226
Priscila Leite <pricila135leite@gmail.com>
2023-05-12 18:04:05 -0300

    Criando o método ActionResult  [HttpPost]

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,36 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>>GetActionResult()
+        {
+            var categoria = _context.Categorias.ToList();
+            if(categoria == null)
+            {
+                return NotFound("Categoria não encontrada.");
+            }
+            
+            return Ok(categoria);
+        
+        }
+    
+
+    
+    
+    }
+}
